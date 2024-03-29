5d1e813878653f759da93299952a781eeffc7cb4
emiraelDev <emmzex19@gmail.com>
2024-03-29 13:58:58 +0100

    add:nav buttons

--- src/components/slider/Carousel.tsx
+++ src/components/slider/Carousel.tsx
@@ -3,6 +3,10 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import styles from "./Slider.module.css";
 
+interface data {
+  image: string;
+  description: string;
+}
 export default function Carousel() {
   let settings = {
     dots: true,
@@ -15,7 +19,7 @@ export default function Carousel() {
     pauseOnHover: true,
     cssEase: "linear",
   };
-  const datas = [
+  const datas: data[] = [
     {
       image: styles.imageOne,
       description: "Style your way to the top",
@@ -30,14 +34,14 @@ export default function Carousel() {
     },
   ];
   return (
-    <div>
-      <Slider {...settings} className=" overflow-hidden">
+    <div className="  m-auto w-[100%] overflow-auto">
+      <Slider {...settings} className="overflow-hidden">
         {datas.map((data, index) => (
           <div
             key={index}
             className={`h-[45vh] md:h-[50vh] lg:h-[60vh] ${data.image} bg-orange-200 bg-blend-multiply relative bg-no-repeat bg-cover bg-center`}
           >
-            <p className=" text-gray-100 text-center font-bold tracking-wider text-xl md:text-2xl lg:text-3xl">
+            <p className=" pt-5 text-gray-100 text-center font-bold tracking-wider text-xl md:text-2xl lg:text-3xl">
               {data.description}
             </p>
           </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
+import NavButtons from "../components/NavButtons";
 import Carousel from "../components/slider/Carousel";
-
 export default function Home() {
   return (
-    <div className=" text-red-400 pt-36">
+    <div className="  pt-36">
       <Carousel />
+      <NavButtons />
     </div>
   );
 }
