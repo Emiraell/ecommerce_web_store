63b77fe416290b8ce9db4d597f20ad4064ef8a6d
emiraelDev <emmzex19@gmail.com>
2024-04-02 02:06:25 +0100

    add: shop all content

--- src/pages/shopAll/Product.tsx
+++ src/pages/shopAll/Product.tsx
@@ -1,3 +1,71 @@
-export default function Product() {
-  return <div>Product</div>;
+import { faMinus, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+export default function Product({ product }: any) {
+  return (
+    <div>
+      <div key={product.id} className=" shadow-lg">
+        <Link to={`/product/${product.id}`}>
+          <div className="relative">
+            <img
+              src={product.images[0]}
+              alt=""
+              className="rounded-lg h-48 object-contain w-full"
+            />{" "}
+            <p className=" absolute top-14 right-3 text-red-700 text-3xl font-bold uppercase -rotate-45">
+              Sale%
+            </p>
+          </div>
+          <div className="text-center text-xl font-bold md:h-16 mt-4 ">
+            <span>{product.title}</span>{" "}
+            <FontAwesomeIcon icon={faMinus} className="px-2" />
+            <span>{product.brand}</span>
+          </div>
+        </Link>
+        <p className="text-center px-3 md:h-32 text-lg">
+          {product.description}
+        </p>
+
+        <div className="flex justify-around">
+          <p className=" text-xl font-bold text-emerald-900 ">
+            ${product.price}
+          </p>{" "}
+          <div className=" text-orange-400">
+            <p>{product.rating < 2 && <FontAwesomeIcon icon={faStar} />}</p>
+            <p>
+              {product.rating < 3 && (
+                <span>
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                </span>
+              )}
+            </p>
+            <p>
+              {product.rating < 4 && (
+                <span>
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                </span>
+              )}
+            </p>
+            <p>
+              {product.rating < 5 && (
+                <span>
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                </span>
+              )}
+            </p>
+          </div>
+        </div>
+        <button className=" w-full my-5 bg-yellow-500 p-3 rounded-full ">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/pages/shopAll/ShopAll.tsx
+++ src/pages/shopAll/ShopAll.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useAppSelector } from "../../store/Store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faStar } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import Product from "./Product";
 
 export default function ShopAll() {
   const [laptops, setLaptops] = useState<any[] | null>();
@@ -36,67 +38,59 @@ export default function ShopAll() {
     assignProducts();
   }, []);
   return (
-    <div className="pt-48 w-[90%] m-auto text-lg  tracking-wider">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {phones?.map((phone) => (
-          <div key={phone.id} className=" shadow-md">
-            <div className="relative">
-              <img
-                src={phone.images[0]}
-                alt=""
-                className="rounded-lg h-48 object-contain w-full"
-              />{" "}
-              <p className=" absolute top-14 right-3 text-red-700 text-3xl font-bold uppercase -rotate-45">
-                Sale%
-              </p>
-            </div>
-            <div className="py-5 text-center text-xl font-bold ">
-              <span>{phone.title}</span>{" "}
-              <FontAwesomeIcon icon={faMinus} className="px-2" />
-              <span>{phone.brand}</span>
-            </div>
-            <p className="text-center px-3 md:h-32">{phone.description}</p>
+    <div className="pt-44 w-[90%] m-auto text-lg  tracking-wider">
+      <div>
+        <p className=" border-b border-green-500 mb-5 ">Phone</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {phones?.map((phone) => (
+            <Product product={phone} />
+          ))}
+        </div>
+      </div>
 
-            <div className="flex justify-around">
-              <p className=" text-xl font-bold text-emerald-900 ">
-                ${phone.price}
-              </p>{" "}
-              <div className=" text-orange-400">
-                <p>{phone.rating < 2 && <FontAwesomeIcon icon={faStar} />}</p>
-                <p>
-                  {phone.rating < 3 && (
-                    <span>
-                      <FontAwesomeIcon icon={faStar} />
-                      <FontAwesomeIcon icon={faStar} />
-                    </span>
-                  )}
-                </p>
-                <p>
-                  {phone.rating < 4 && (
-                    <span>
-                      <FontAwesomeIcon icon={faStar} />
-                      <FontAwesomeIcon icon={faStar} />
-                      <FontAwesomeIcon icon={faStar} />
-                    </span>
-                  )}
-                </p>
-                <p>
-                  {phone.rating < 5 && (
-                    <span>
-                      <FontAwesomeIcon icon={faStar} />
-                      <FontAwesomeIcon icon={faStar} />
-                      <FontAwesomeIcon icon={faStar} />
-                      <FontAwesomeIcon icon={faStar} />
-                    </span>
-                  )}
-                </p>
-              </div>
-            </div>
-            <button className=" w-full my-5 bg-yellow-500 p-3 rounded-full ">
-              Add to Cart
-            </button>
-          </div>
-        ))}
+      <div className="mt-16">
+        <p className=" border-b border-green-500 mb-5 ">Laptops</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {laptops?.map((laptop) => (
+            <Product product={laptop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <p className=" border-b border-green-500 mb-5 ">Home Decoration</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {homeDecorations?.map((decoration) => (
+            <Product product={decoration} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <p className=" border-b border-green-500 mb-5 ">Skincare</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {skincares?.map((skincare) => (
+            <Product product={skincare} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <p className=" border-b border-green-500 mb-5 ">Fragrances</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {fragrances?.map((fragrance) => (
+            <Product product={fragrance} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <p className=" border-b border-green-500 mb-5 ">Groceries</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {groceries?.map((grocery) => (
+            <Product product={grocery} />
+          ))}
+        </div>
       </div>
     </div>
   );
